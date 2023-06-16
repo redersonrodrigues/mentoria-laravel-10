5b47ad5639936b2e51a61d456ede2b65a2bf5760
redersonrodrigues <r.ataliba@hotmail.com>
2023-06-16 18:31:00 -0300

    referenciar rota no front

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProdutosController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('index');
+});
+// Produtos
+Route::prefix('produtos')->name('produtos.')->controller(ProdutosController::class)->group(function (){
+    Route::get('/',  'index')->name('index');
 });
